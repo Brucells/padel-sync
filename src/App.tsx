c78141a8cf5fa1e8693db09fcecbdd93e1dcdde8
Brucells <brucells@hotmail.com>
2024-09-20 16:02:36 +0200

    Implement app template

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,127 @@
-import { Button } from "./components/ui/button"
-
+import {
+  Camera,
+  ChartColumnBig,
+  ChartSpline,
+  Database,
+  Diameter,
+  Dribbble,
+  Home,
+  User,
+  Users,
+} from "lucide-react"
+import { Badge } from "@/components/ui/badge"
+import { Button } from "@/components/ui/button"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import Header from "./components/Header"
+import { ThemeProvider } from "./components/ThemeProvider"
 
 function App() {
-
   return (
-    <>
-      <Button>Click</Button>
-    </>
+    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+      <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+        <div className="hidden border-r bg-muted/40 md:block">
+          <div className="flex h-full max-h-screen flex-col gap-2">
+            <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
+              <a href="/" className="flex items-center gap-2 font-semibold">
+                <Diameter className="h-6 w-6" />
+                <span className="">PadelSync</span>
+              </a>
+              <Avatar className="ml-auto h-8 w-8">
+                <AvatarImage src="avatar.jpg" />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+              
+            </div>
+            <div className="flex-1">
+              <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
+                <a
+                  href="#"
+                  className="flex items-center gap-3 rounded-lg bg-muted px-3 py-2 text-primary transition-all hover:text-primary"
+                >
+                  <Home className="h-4 w-4" />
+                  Dashboard
+                </a>
+                <a
+                  href="#"
+                  className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+                >
+                  <Dribbble className="h-4 w-4" />
+                  Match
+                </a>
+                <a
+                  href="#"
+                  className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+                >
+                  <ChartColumnBig className="h-4 w-4" />
+                  Statistiques
+                </a>
+                <a
+                  href="#"
+                  className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+                >
+                  <ChartSpline className="h-4 w-4" />
+                  Progression
+                </a>
+                <a
+                  href="#"
+                  className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+                >
+                  <Camera className="h-4 w-4" />
+                  Videos
+                </a>
+                <a
+                  href="#"
+                  className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+                >
+                  <Database className="h-4 w-4" />
+                  Données
+                  
+                </a>
+                <a
+                  href="#"
+                  className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+                >
+                  <Users className="h-4 w-4" />
+                  Coéquipiers
+                  <Badge className="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full">
+                    3
+                  </Badge>
+                </a>
+                <a
+                  href="#"
+                  className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+                >
+                  <User className="h-4 w-4" />
+                  Profil
+                </a>
+              </nav>
+            </div>
+            
+          </div>
+        </div>
+        <div className="flex flex-col">
+          <Header />
+          <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
+            <div className="flex items-center">
+              <h1 className="text-lg font-semibold md:text-2xl">Dashboard</h1>
+            </div>
+            <div
+              className="flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm" x-chunk="dashboard-02-chunk-1"
+            >
+              <div className="flex flex-col items-center gap-1 text-center">
+                <h3 className="text-2xl font-bold tracking-tight">
+                  You have no products
+                </h3>
+                <p className="text-sm text-muted-foreground">
+                  You can start selling as soon as you add a product.
+                </p>
+                <Button className="mt-4">Add Product</Button>
+              </div>
+            </div>
+          </main>
+        </div>
+      </div>
+    </ThemeProvider>
   )
 }
 
